54bbb4426332b623d065e2dfaa20e4da3c90ebca
Gabriel Baldi <gabriel@espacoalfavita.com.br>
2025-04-17 16:20:48 -0300

    Site Angular 1.0

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="text-align: center; padding: 20px">
+    <h1>ClickMaster 3000</h1>
+    <h2>{{ contador }}</h2>
+    <button @click="incrementar">+1</button>
+    <button @click="decrementar">-1</button>
+    <button @click="resetar">Resetar</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+
+const contador = ref(parseInt(localStorage.getItem('contador')) || 0);
+
+watch(contador, (novo) => {
+  localStorage.setItem('contador', novo);
+});
+
+const incrementar = () => contador.value++;
+const decrementar = () => contador.value--;
+const resetar = () => contador.value = 0;
+</script>
